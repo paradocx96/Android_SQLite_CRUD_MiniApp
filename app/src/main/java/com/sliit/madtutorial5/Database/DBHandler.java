9f881b81e700e4bef0f0c80656de22a628b07bc3
Navinda Lankesh <47824016+paradocx96@users.noreply.github.com>
2020-11-08 15:01:15 +0530

    CRUD Functions Finished. Login Function Added!

--- app/src/main/java/com/sliit/madtutorial5/Database/DBHandler.java
+++ app/src/main/java/com/sliit/madtutorial5/Database/DBHandler.java
@@ -5,12 +5,15 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class DBHandler extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "UserInfo.db";
+    private static final String TAG = "DBHandler";
 
     public DBHandler(Context context) {
         super(context, DATABASE_NAME, null,1);
@@ -32,7 +35,7 @@ public class DBHandler extends SQLiteOpenHelper {
 
     }
 
-    public void addInfo (String username, String password) {
+    public long addInfo (String username, String password) {
         SQLiteDatabase db = getWritableDatabase();
         ContentValues values = new ContentValues();
 
@@ -40,6 +43,8 @@ public class DBHandler extends SQLiteOpenHelper {
         values.put(UsersMaster.Users.COLUMN_PASSWORD, password);
 
         long result = db.insert(UsersMaster.Users.TABLE_NAME, null, values);
+
+        return result;
     }
 
     public List readAllInfo() {
@@ -77,16 +82,63 @@ public class DBHandler extends SQLiteOpenHelper {
         return USERNAME;
     }
 
-    public void deleteInfo (String username) {
+    public List readUserInfo(String request) {
+        SQLiteDatabase db = getReadableDatabase();
+
+        String[] projection = {
+                UsersMaster.Users._ID,
+                UsersMaster.Users.COLUMN_USERNAME,
+                UsersMaster.Users.COLUMN_PASSWORD
+        };
+
+        String sortOrder = UsersMaster.Users.COLUMN_USERNAME + " DESC";
+
+        Cursor cursor = db.query(
+                UsersMaster.Users.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                sortOrder
+        );
+
+        List USERNAME = new ArrayList<>();
+        List PASSWORD = new ArrayList<>();
+
+        while (cursor.moveToNext()) {
+            String username = cursor.getString(cursor.getColumnIndexOrThrow(UsersMaster.Users.COLUMN_USERNAME));
+            String password = cursor.getString(cursor.getColumnIndexOrThrow(UsersMaster.Users.COLUMN_PASSWORD));
+            USERNAME.add(username);
+            PASSWORD.add(password);
+        }
+
+        cursor.close();
+        Log.i(TAG,"readUserInfo : " + USERNAME);
+
+        if (request == "User" || request == "user") {
+            return USERNAME;
+        }
+        else if (request == "Password" || request == "password") {
+            return PASSWORD;
+        }
+        else {
+            return null;
+        }
+    }
+
+    public int deleteInfo (String username) {
         SQLiteDatabase db = getReadableDatabase();
 
         String selection = UsersMaster.Users.COLUMN_USERNAME + " LIKE ?";
         String[] selectionArgs = {username};
 
-        db.delete(UsersMaster.Users.TABLE_NAME, selection, selectionArgs);
+        int result = db.delete(UsersMaster.Users.TABLE_NAME, selection, selectionArgs);
+
+        return result;
     }
 
-    public void updateInfo (String username, String password) {
+    public int updateInfo (String username, String password) {
         SQLiteDatabase db = getReadableDatabase();
         ContentValues values = new ContentValues();
 
@@ -96,5 +148,7 @@ public class DBHandler extends SQLiteOpenHelper {
         String[] selectionArgs = {username};
 
         int result = db.update(UsersMaster.Users.TABLE_NAME, values, selection, selectionArgs);
+
+        return result;
     }
 }
